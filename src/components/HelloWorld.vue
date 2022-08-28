e6cacaff2025e048690ca6ffa3b031513f31e775
Sergey <owlbert@null.net>
2022-08-28 18:53:14 +0700

    form_done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,206 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="form">
+    <Select v-for="(param, index) in selectedParams" :key="index" :selectedParam="param" :optionValues="selectData" :selectId="index" @changeValue="changeParams" />
+    <button @click="saveData">Сохранить</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
+import Select from "./Select.vue";
 export default {
   name: 'HelloWorld',
+  components: {
+    Select
+  },
   props: {
-    msg: String
+
+  },
+  setup() {
+    const DATA = [
+      {
+        select_city: "Novosibirsk",
+        select_ceh: "ceh9",
+        select_manager: ["Vadim", "Marina"],
+      },
+      {
+        select_city: "Novosibirsk",
+        select_ceh: "ceh4",
+        select_manager: ["Alex", "Max"],
+      },
+      {
+        select_city: "Omsk",
+        select_ceh: "ceh1",
+        select_manager: ["Alex", "Leo", "Artem"],
+      },
+      {
+        select_city: "Kazan",
+        select_ceh: "ceh",
+        select_manager: ["Sergey"],
+      },
+    ];
+    const ALL_CREW = [
+      {
+        select_CREW: "Novosibirsk_NightCrew_One",
+        select_city: "Novosibirsk",
+        select_workTime: "20:00 - 08:00",
+      },
+      {
+        select_CREW: "Novosibirsk_NightCrew_Two",
+        select_city: "Novosibirsk",
+        select_workTime: "20:00 - 08:00",
+      },
+      {
+        select_CREW: "Novosibirsk_DayCrew_One",
+        select_city: "Novosibirsk",
+        select_workTime: "08:00 - 20:00",
+      },
+      {
+        select_CREW: "Novosibirsk_DayCrew_Two",
+        select_city: "Novosibirsk",
+        select_workTime: "08:00 - 20:00",
+      },
+      {
+        select_CREW: "Omsk_NightCrew_One",
+        select_city: "Omsk",
+        select_workTime: "20:00 - 08:00",
+      },
+      {
+        select_CREW: "Omsk_NightCrew_Two",
+        select_city: "Omsk",
+        select_workTime: "20:00 - 08:00",
+      },
+      {
+        select_CREW: "Omsk_DayCrew_One",
+        select_city: "Omsk",
+        select_workTime: "08:00 - 20:00",
+      },
+      {
+        select_CREW: "Omsk_DayCrew_Two",
+        select_city: "Omsk",
+        select_workTime: "08:00 - 20:00",
+      },
+      {
+        select_CREW: "Kazan_NightCrew_One",
+        select_city: "Kazan",
+        select_workTime: "20:00 - 08:00",
+      },
+      {
+        select_CREW: "Kazan_NightCrew_Two",
+        select_city: "Kazan",
+        select_workTime: "20:00 - 08:00",
+      },
+      {
+        select_CREW: "Kazan_DayCrew_One",
+        select_city: "Kazan",
+        select_workTime: "08:00 - 20:00",
+      },
+      {
+        select_CREW: "Kazan_DayCrew_Two",
+        select_city: "Kazan",
+        select_workTime: "08:00 - 20:00",
+      },
+    ];
+
+    const selectedParams = ref({
+      select_city: "",
+      select_ceh: "",
+      select_manager: "",
+      select_workTime: "",
+      select_CREW: "",
+    })
+    getParamsFromCookie(selectedParams)
+
+    const selectData = ref({
+      select_city: [],
+      select_ceh: [],
+      select_manager: [],
+      select_workTime: [],
+      select_CREW: [],
+    })
+
+    getData(selectData, selectedParams)
+
+    function getData(output, params) {
+      output.value.select_city = getFormatedData('city', params)
+      output.value.select_ceh = getFormatedData('ceh', params)
+      output.value.select_manager = getFormatedData('manager', params)
+      output.value.select_workTime = getFormatedData('workTime', params)
+      output.value.select_CREW = getFormatedData('crew', params)
+    }
+
+
+    function getFormatedData(id, params) {
+      switch (id) {
+        case 'city':
+          return DATA.map((el) => el.select_city).filter((el, index, array) => array.indexOf(el) == index)
+
+        case 'ceh':
+          return DATA.filter((el) => params.value.select_city != '' ? el.select_city == params.value.select_city : el).map((el) => el.select_ceh)
+
+        case 'manager':
+          return DATA
+            .filter((el) => params.value.select_city != '' ? el.select_city == params.value.select_city : el)
+            .filter((el) => params.value.select_ceh != '' ? el.select_ceh == params.value.select_ceh : el)
+            .map((el) => el.select_manager)
+            .flat()
+
+        case 'workTime':
+          return ALL_CREW.map((el) => el.select_workTime).filter((el, index, array) => array.indexOf(el) == index)
+
+        case 'crew':
+          return ALL_CREW
+            .filter((el) => params.value.select_city != '' ? el.select_city == params.value.select_city : el)
+            .filter((el) => params.value.select_workTime != '' ? el.select_workTime == params.value.select_workTime : el)
+            .map((el) => el.select_CREW)
+
+        default:
+          break;
+      }
+    }
+
+    function changeParams(elem){
+      if (elem.id === 'select_city') {
+        setDefault()
+      }
+      selectedParams.value[elem.id] = elem.value
+      getData(selectData, selectedParams)
+    }
+
+    function setDefault() {
+      selectedParams.value.select_CREW=''
+      selectedParams.value.select_ceh=''
+      selectedParams.value.select_manager=''
+      selectedParams.value.select_workTime=''
+    }
+
+    function getParamsFromCookie(output) {
+      output.value
+      if (document.cookie !== '') {
+        output.value = JSON.parse(document.cookie)
+      }
+    }
+
+    function saveData(){
+      document.cookie = JSON.stringify(selectedParams.value)
+    }
+    return {
+      selectedParams,
+      selectData,
+      ALL_CREW,
+      saveData,
+      changeParams
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="formBody">
+    <select class="formSelect" :name="selectId" :id="selectId" @change="$emit('changeValue', $event.target)" v-model="defaultValue"> 
+      <option value="">Select value</option>
+      <option v-for="(opt, index) in optionValues[selectId]" :key="index" :value="opt">{{opt}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { toRef } from '@vue/reactivity'
+export default {
+name:'select-view',
+props:{
+  selectId: {
+    type: String
+  },
+  optionValues: {
+    type: Object
+  },
+  selectedParam: {
+    type: String
+  }
+},
+setup(props){
+  const defaultValue = toRef(props, 'selectedParam')
+  return {
+    defaultValue
+  }
+}
+}
+</script>
+
+<style scoped>
+.formBody{
+    display: flex;
+    justify-content: center;
+    width: 50%;
+  }
+.formSelect{
+  padding: 5px 10px;
+  margin-bottom: 10px;
+  width: 100%;
+}
+</style>
